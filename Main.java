b8d326a70a8dde9773066aa9439e07cd287dfa67
Nic Wiest <nicwiest@gmail.com>
2018-08-20 17:25:17 -0400

    Add files via upload
    Just copied the text from the bottom of repl.it

---
+++ Main.java
@@ -0,0 +1,16 @@
+// John Doe
+
+public class Main 
+{
+  
+  public static void main(String[] args) 
+  {
+    System.out.println("hello world");
+    System.out.println("java version 1.8.0_31");
+    System.out.println("Java(TM) SE Runtime Environment (build 1.8.0_31-b13)");
+    System.out.println("Java HotSpot(TM) 64-Bit Server VM (build 25.31-b07, mixed mode)");
+    System.out.println("Copied from the bottom output block :)");
+    System.out.println("Cause I had no idea what to do ");
+  }
+  
+}
